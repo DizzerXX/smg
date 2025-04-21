fca384b19cea60b3dbb0d8044bf2f9bebd8566c2
danish.kas <danish12380@gmail.com>
2025-04-21 23:07:53 +0800

    first push

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Hero from './components/Hero.vue';
+import Header from './components/Header.vue';
+import Exhibitors from './components/Exhibitors.vue';
+
+</script>
+
+<template>
+    <Header/>
+    <Hero
+      title="myStarjob fair 2025"
+      subtitle="As one of Malaysia's largest recruitment event providers, myStarjob Fair has served over 26,000 companies and 120,000 visitors. By exhibiting at our fair you will stand a chance to meet a diverse range of candidates and enhance your corporate branding. Since The Star Newspaper also organises this event, it gives extra mileage in terms of publicity and status as one of the top hiring companies in Malaysia,"
+      cta-text="Explore Now"
+    />
+    <Exhibitors/>
+</template>
+
+<style scoped>
+</style>
